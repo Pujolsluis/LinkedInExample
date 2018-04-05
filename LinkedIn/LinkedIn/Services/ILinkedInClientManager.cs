e0284977e68eb6b69f78ea40ee4019c38b8e306e
Luis Pujols <luispujols@hotmail.com>
2018-04-05 09:57:35 -0400

    Added the LinkedInClientManager interface and created the base files for the implementation in each platform

--- LinkedIn/LinkedIn/Services/ILinkedInClientManager.cs
+++ LinkedIn/LinkedIn/Services/ILinkedInClientManager.cs
@@ -1,10 +1,61 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using LinkedIn.Models;
 
 namespace LinkedIn.Services
 {
+
+    public enum LinkedInActionStatus
+    {
+        Canceled,
+        Unauthorized,
+        Completed,
+        Error
+    }
+
+    public class LinkedInClientResultEventArgs<T> : EventArgs
+    {
+        public T Data { get; set; }
+        public LinkedInActionStatus Status { get; set; }
+        public string Message { get; set; }
+
+        public LinkedInClientResultEventArgs(T data, LinkedInActionStatus status, string msg = "")
+        {
+            Data = data;
+            Status = status;
+            Message = msg;
+        }
+    }
+
+    public class LinkedInResponse<T>
+    {
+        public T Data { get; set; }
+        public LinkedInActionStatus Status { get; set; }
+        public string Message { get; set; }
+
+        public LinkedInResponse(LinkedInClientResultEventArgs<T> evtArgs)
+        {
+            Data = evtArgs.Data;
+            Status = evtArgs.Status;
+            Message = evtArgs.Message;
+        }
+
+        public LinkedInResponse(T user, LinkedInActionStatus status, string msg = "")
+        {
+            Data = user;
+            Status = status;
+            Message = msg;
+        }
+    }
+
     interface ILinkedInClientManager
     {
+        event EventHandler<LinkedInClientResultEventArgs<LinkedInUser>> OnLogin;
+        event EventHandler OnLogout;
+        Task<LinkedInResponse<LinkedInUser>> LoginAsync();
+        void Logout();
+        bool IsLoggedIn { get; }
     }
 }
