c7d4c9c5c8abea9fe0a5e120bbf650827f67806f
Luis Pujols <luispujols@hotmail.com>
2018-04-04 22:04:39 -0400

    Add project files.

---
+++ LinkedIn/LinkedIn/Models/LinkedInUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace LinkedIn.Models
+{
+    public class LinkedInUser : INotifyPropertyChanged
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Uri Picture { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ LinkedIn/LinkedIn/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Input;
+using LinkedIn.Models;
+using Xamarin.Forms;
+
+namespace LinkedIn.ViewModels
+{
+    public class LoginPageViewModel : INotifyPropertyChanged
+    {
+        public LinkedInUser user { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Name
+        {
+            get { return user.Name; }
+            set { user.Name = value; }
+        }
+
+        public string Email
+        {
+            get { return user.Email; }
+            set { user.Email = value; }
+        }
+
+        public Uri Picture
+        {
+            get { return user.Picture; }
+            set { user.Picture = value; }
+        }
+
+        public bool IsLoggedIn { get; set; }
+
+        public ICommand LoginCommand { get; set; }
+        public ICommand LogoutCommand { get; set; }
+        //private IGoogleClientManager googleClientManager;
+
+        public LoginPageViewModel()
+        {
+            LoginCommand = new Command(LoginAsync);
+            LogoutCommand = new Command(Logout);
+            //googleClientManager = CrossGoogleClient.Current;
+            IsLoggedIn = false;
+        }
+
+        public void LoginAsync()
+        {
+            //googleClientManager.OnLogin += OnLoginCompleted;
+            //googleClientManager.LoginAsync();
+        }
+
+
+        //private void OnLoginCompleted(object sender, GoogleClientResultEventArgs<GoogleUser> loginEventArgs)
+        //{
+        //    if (loginEventArgs.Data != null)
+        //    {
+        //        user = loginEventArgs.Data;
+
+        //        // Log the current user email
+        //        Debug.WriteLine(user.Email);
+        //        IsLoggedIn = true;
+        //    }
+        //    else
+        //    {
+        //        App.Current.MainPage.DisplayAlert("Error", loginEventArgs.Message, "OK");
+        //    }
+
+        //    googleClientManager.OnLogin -= OnLoginCompleted;
+
+        //}
+
+        public void Logout()
+        {
+            //googleClientManager.OnLogout += OnLogoutCompleted;
+            //googleClientManager.Logout();
+        }
+
+        private void OnLogoutCompleted(object sender, EventArgs loginEventArgs)
+        {
+            IsLoggedIn = false;
+            user.Email = "Offline";
+            //googleClientManager.OnLogout -= OnLogoutCompleted;
+        }
+
+    }
+}
+
