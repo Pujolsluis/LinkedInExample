9ddae452011c8d249dfd34c2a55ebbdb3dc25445
Luis Pujols <luispujols@hotmail.com>
2018-05-25 01:11:00 -0400

    Added the base exceptions to the LinkedIn Project

---
+++ LinkedIn/LinkedIn/Services/LinkedInClientException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace LinkedIn.Services
+{
+	public class  LinkedInClientBaseException : Exception
+    {
+        public static string SignInDefaultErrorMessage = "The LinkedIn Sign In could not complete it's process correctly.";
+
+		public LinkedInClientBaseException() : base() { }
+		public LinkedInClientBaseException(string message) : base(message) { }
+		public LinkedInClientBaseException(string message, System.Exception inner) : base(message, inner) { }
+    }
+}
